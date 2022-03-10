b9a8ef045b27193a864a45d6eca595191c410e52
Houdaïfa Mofril <houdaifadevelopper@gmail.com>
2022-03-10 15:56:09 +0100

    Refonte du design de l'application, ajout de styles, mobile first design

--- frontend/index.js
+++ frontend/index.js
@@ -1,39 +1,37 @@
-( async function(){
+(async function () {
     const teddies = await getTeddies();
 
-    for(teddy of teddies) {
+    for (teddy of teddies){
         displayTeddies(teddy);
     }
+
 }());
 
-
 function getTeddies() {
-    return fetch(" http://localhost:3000/api/teddies")
-        .then(function(httpBodyResponse){
+    return fetch("http://localhost:3000/api/teddies")
+        .then(function (httpBodyResponse){
             return httpBodyResponse.json()
         })
-        .then(function (teddies){
-            return teddies
-
+        .then(function(teddies){
+           return teddies
         })
         .catch(function (error){
             return error
-        });
-};
+        })
+}
 
 function displayTeddies() {
-
-    const templateElt = document.getElementById("templateTeddy");
+    const templateElt = document.getElementById("template__element")
     const cloneElt = document.importNode(templateElt.content, true);
 
-    cloneElt.getElementById("ours__name").textContent = teddy.name;
-    cloneElt.getElementById("ours__image").src = teddy.imageUrl;
-    cloneElt.getElementById("ours__image").alt = teddy.name;
-    cloneElt.getElementById("ours__price").textContent = teddy.price/100 + "€";
-    cloneElt.getElementById("ours__link").href = `id=${teddy._id}` ;
+    cloneElt.getElementById("ours__nom").textContent = teddy.name
+    cloneElt.getElementById("ours__image").src = teddy.imageUrl
+    cloneElt.getElementById("ours__prix").textContent = teddy.price/100 + "€"
+    cloneElt.getElementById("ours__link").textContent = " Voir " + teddy.name
+    cloneElt.getElementById("ours__link").href = `id=${teddy._id}`
+
+    document.getElementById("main").appendChild(cloneElt)
 
 
 
-    document.getElementById("ours__container").appendChild(cloneElt);
-
-};
+}
