385ffb7c3d0ce17d143628cdb3cda441c658b409
Houdaïfa Mofril <houdaifadevelopper@gmail.com>
2022-03-13 22:39:59 +0100

    Ajout complexité function localStorage(), modification tag option

--- frontend/teddy.js
+++ frontend/teddy.js
@@ -1,4 +1,5 @@
 (async function(){
+
     const teddyId = getTeddyId()
     const teddy = await getTeddy(teddyId)
     hydrateTeddy(teddy)
@@ -7,10 +8,12 @@
 })()
 
 function getTeddyId(){
+
     return new URL(location.href).searchParams.get("id")
 }
 
 function getTeddy(teddyId){
+
     return fetch(`http://localhost:3000/api/teddies/${teddyId}`)
         .then(function (httpBodyResponse){
             return httpBodyResponse.json()
@@ -22,68 +25,66 @@ function getTeddy(teddyId){
         .catch(function (error){
             return error
         })
-}
+};
 
 function hydrateTeddy(teddy){
 
-
     const teddyColors = teddy.colors;
-
     teddyColors.forEach(color =>{
-        
-        document.getElementById("color__select").innerHTML += `<option value=${color} >${color}</option>`
-        
+    const optionTag = document.createElement("option");
+    document.getElementById("color__select").appendChild(optionTag)
+    optionTag.innerHTML += `<option value=${color} selected >${color}</option>`  
     });
-    
-    
-
-
     document.getElementById("ours__nom").textContent = teddy.name
     document.getElementById("ours__image").src = teddy.imageUrl
     document.getElementById("ours__prix").textContent = teddy.price/100 + "€"
-    
-}
+};
 
 function calculPrixPanier(teddy){
 
     const priceTeddy = teddy.price;
-
-
     const quantityTeddy = document.getElementById("quantity");
     const totalPrice = document.getElementById("total__prix");
-
     quantityTeddy.addEventListener("change", (event)=> {
         totalPrice.textContent = `Total: ` + (priceTeddy * `${event.target.value}`)/100 + "€";
     })
+};
 
-}
+
+
 
 function ajoutLocalStorage(teddy) {
-    const cart = JSON.parse(localStorage.getItem("cart")) || [];
     const buttonCart = document.getElementById("link__ajout__panier");
+
+    buttonCart.addEventListener("click", (event) => {  
+
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    console.log(cart)
     const quantityTeddy = document.getElementById("quantity");
     const colorTeddyStorage = document.getElementById("color__select");
-    
-    
-    
-    buttonCart.addEventListener("click", (event)=>{
-        console.log(teddy)
-    
-        let cartObject = {
-            ID: `${teddy._id}`,
-            Name: `${teddy.name}`, 
-            Quantity: `${quantityTeddy.value}`,
-            Color: `${colorTeddyStorage.value}`,
-            Image: `${teddy.imageUrl}`
-        };
-        console.log(colorTeddyStorage.value);
 
+    const oldteddy = cart.find((item) => item._id == teddy._id && item.color == colorTeddyStorage.value);
+
+    if (oldteddy != undefined) {
+        oldteddy.quantity = parseInt(oldteddy.quantity) + parseInt(quantityTeddy.value);
+    } 
+    else {
+
+        let cartObject = {
+            _id: `${teddy._id}`,
+            name: `${teddy.name}`, 
+            quantity: `${quantityTeddy.value}`,
+            color: `${colorTeddyStorage.value}`,
+            image: `${teddy.imageUrl}`
+        };
         cart.push(cartObject);
 
-        localStorage.setItem("cart", JSON.stringify(cart));
-        
-        
-    })
-    console.log(cart)
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+    event.preventDefault();
+    window.location.href = `panier.html`
+    
 
+    });
+     
 };
