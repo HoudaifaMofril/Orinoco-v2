d9e0be454ace72ed728230fcabd37ca30d6bcb06
Houdaïfa Mofril <houdaifadevelopper@gmail.com>
2022-03-18 00:35:34 +0100

    Ajout fonction pour afficher dynmaiquement les infos de la commande ou un message de non-commande

--- frontend/confirm.js
+++ frontend/confirm.js
@@ -1,11 +1,19 @@
+orderConfirm();
 localStorage.clear();
 
-const getParams = new URL(location.href).searchParams;
-const orderId = getParams.get("order_number");
-const orderPrice = getParams.get("order_price");
 
-console.log(orderId)
-console.log(orderPrice)
+function orderConfirm(){
+    if (localStorage.length == 0){
+        document.getElementById("main").innerHTML = `<p id="infos__order">Aucune commande en cours</p>`
+    } else {
+        const getParams = new URL(location.href).searchParams;
+        const orderId = getParams.get("order_number");
+        const orderPrice = getParams.get("order_price");
+        document.getElementById("order__number").textContent = orderId;
+        document.getElementById("order__price").textContent = orderPrice + " euros ";
 
-document.getElementById("order__number").textContent = orderId;
-document.getElementById("order__price").textContent = orderPrice + " euros ";
+    }
+
+}
+
+
